78943b483632d0f796402265438ba21ba839022f
Aditya Barad <adityabarad2022@gmail.com>
2026-04-03 17:18:32 +0530

    part2 completed

---
+++ src/main/java/com/example/zorvyn/dto/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.example.zorvyn.dto.response;
+
+public record AuthResponse(
+        String accessToken,
+        String refreshToken,
+        String tokenType,
+        long expiresIn
+) {
+}
+
+
+
